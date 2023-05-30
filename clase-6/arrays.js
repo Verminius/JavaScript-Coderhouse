13cfbff6d117a5157cfc28d21eb501a24d0e7acc
Alexander Patiño <alpatino.ortiz@gmail.com>
2023-05-29 22:07:38 -0500

    agregados ejercicios de clase 5: Objetos

---
+++ clase-6/arrays.js
@@ -0,0 +1,12 @@
+//listas
+
+const listaFestejo = ["bebidas", "salchichas", "pan", "torta", "mayonesa", "vodka"]
+
+console.log(listaFestejo);
+console.log("en la primer posición tenemos " + listaFestejo[0]);
+console.log(listaFestejo.length);
+
+for (let i = 0; i <= listaFestejo.length; i++) {
+    console.log(listaFestejo[i]);;
+
+}
