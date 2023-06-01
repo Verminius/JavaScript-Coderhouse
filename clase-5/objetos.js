206dbc732c77ad900a19248544b316c67a8383da
Alexander Patiño <alpatino.ortiz@gmail.com>
2023-06-01 13:47:53 -0500

    Completos Ejercicios clase 6

--- clase-5/objetos.js
+++ clase-5/objetos.js
@@ -2,26 +2,26 @@
 // ---------Literales
 
 const personaje = {
-    id: 01,
-    name: "Belenus",
-    background: "Charlatan",
-    str: 10,
-    dex: 14,
-    con: 18,
-    int: 16,
-    wis: 12,
-    cha: 18,
-}
+  id: 01,
+  name: "Belenus",
+  background: "Charlatan",
+  str: 10,
+  dex: 14,
+  con: 18,
+  int: 16,
+  wis: 12,
+  cha: 18,
+};
 
 //Llamar Objeto
-console.log(personaje)
+console.log(personaje);
 
 //Llamar propiedad de Objeto
-console.log(personaje.background)
+console.log(personaje.background);
 
 //Modificar propiedad dentro de Objeto
 personaje.str = 12;
-console.log(personaje)
+console.log(personaje);
 
 //Otra forma de cambiar propiedades del Objeto
 personaje["str"] = 14;
@@ -29,99 +29,171 @@ console.log(personaje);
 
 //Agregar nueva propiedad a Objeto
 personaje.lvl = 1;
-console.log(personaje)
+console.log(personaje);
 
 //Eliminar propiedad a Objeto
 delete personaje.id;
-console.log(personaje)
+console.log(personaje);
 
 //Agregando propiedades de Objeto a String
-console.log("Tu personaje " + personaje.name + " es de nivel " + personaje.lvl)
-console.log("Tu personaje " + personaje.name + " tiene un Carisma igual a " + personaje.cha)
+console.log("Tu personaje " + personaje.name + " es de nivel " + personaje.lvl);
+console.log(
+  "Tu personaje " +
+    personaje.name +
+    " tiene un Carisma igual a " +
+    personaje.cha
+);
 
 // ---------Constructores
 //Sintaxis
-function Personaje(id, name, cla, lvl, background, str, dex, con, int, wis, cha) {
-    this.id = id;
-    this.name = name;
-    this.cla = cla;
-    this.lvl = lvl;
-    this.background = background;
-    this.str = str;
-    this.dex = dex;
-    this.con = con;
-    this.int = int;
-    this.wis = wis;
-    this.cha = cha;
+function Personaje(
+  id,
+  name,
+  cla,
+  lvl,
+  background,
+  str,
+  dex,
+  con,
+  int,
+  wis,
+  cha
+) {
+  this.id = id;
+  this.name = name;
+  this.cla = cla;
+  this.lvl = lvl;
+  this.background = background;
+  this.str = str;
+  this.dex = dex;
+  this.con = con;
+  this.int = int;
+  this.wis = wis;
+  this.cha = cha;
 
-    //Metodo
+  //Metodo
 
-    this.describir = function() {
-        console.log("¡Hola extraño! Mi nombre es " + this.name + " y soy un " + this.cla + " " + this.background + " de nivel " + this.lvl + ".")
-    }
+  this.describir = function () {
+    console.log(
+      "¡Hola extraño! Mi nombre es " +
+        this.name +
+        " y soy un " +
+        this.cla +
+        " " +
+        this.background +
+        " de nivel " +
+        this.lvl +
+        "."
+    );
+  };
 }
 
 //Nuevas instancias
-const personaje1 = new Personaje(02, "Verminius", "Barbaro", 4, "Soldado", 19, 14, 16, 11, 10, 8);
-console.log(personaje1)
+const personaje1 = new Personaje(
+  02,
+  "Verminius",
+  "Barbaro",
+  4,
+  "Soldado",
+  19,
+  14,
+  16,
+  11,
+  10,
+  8
+);
+console.log(personaje1);
 
-const personaje2 = new Personaje(03, "Baltazar", "Bardo", 6, "Artista", 10, 16, 18, 8, 12, 20);
-console.log(personaje2)
+const personaje2 = new Personaje(
+  03,
+  "Baltazar",
+  "Bardo",
+  6,
+  "Artista",
+  10,
+  16,
+  18,
+  8,
+  12,
+  20
+);
+console.log(personaje2);
 
-const personaje3 = new Personaje(04, "Nathaniel", "Paladin", 4, "Embrujado", 20, 14, 16, 9, 10, 16);
-console.log(personaje3)
+const personaje3 = new Personaje(
+  04,
+  "Nathaniel",
+  "Paladin",
+  4,
+  "Embrujado",
+  20,
+  14,
+  16,
+  9,
+  10,
+  16
+);
+console.log(personaje3);
 
 //Metodos de Strings
 let oracion = "Tu personaje camina por el pantano";
-console.log(oracion.toLowerCase()) //Todo a minuscula
-console.log(oracion.toUpperCase()) //Todo a mayuscula
+console.log(oracion.toLowerCase()); //Todo a minuscula
+console.log(oracion.toUpperCase()); //Todo a mayuscula
 
-console.log(oracion.length) //longitud del string
+console.log(oracion.length); //longitud del string
 
 //Viendo metodo en objeto creado
-personaje1.describir()
-personaje2.describir()
-personaje3.describir()
+personaje1.describir();
+personaje2.describir();
+personaje3.describir();
 
 // for-in
 for (const prop in personaje1) {
-    console.log(prop, personaje1[prop])
+  console.log(prop, personaje3[prop]);
 }
-
 
 // ---------Clases
 class Weapon {
+  //Propiedades
+  constructor(name, weaponType, dmg, dmgType) {
+    this.name = name.toUpperCase();
+    this.weaponType = weaponType;
+    this.dmg = dmg;
+    this.dmgType = dmgType;
+    this.vinculada = true;
+  }
 
-    //Propiedades
-    constructor(name, weaponType, dmg, dmgType) {
-        this.name = name.toUpperCase();
-        this.weaponType = weaponType;
-        this.dmg = dmg;
-        this.dmgType = dmgType;
-        this.vinculada = true;
-    }
+  //Métodos
+  presentar() {
+    console.log(
+      "Esta es la poderosa " +
+        this.name +
+        " una " +
+        this.weaponType +
+        " capás de hacer un daño de " +
+        this.dmg +
+        " de daño " +
+        this.dmgType +
+        "."
+    );
+  }
 
+  vincular() {
+    this.vinculada = true;
+  }
 
-    //Métodos
-    presentar() {
-        console.log("Esta es la poderosa " + this.name + " una " + this.weaponType + " capás de hacer un daño de " + this.dmg + " de daño " + this.dmgType + ".")
-    }
+  atacar() {
+    console.log(Math.floor(Math.random() * 20 + 1));
+  }
 
-    vincular() {
-        this.vinculada = true
-    }
-
-    atacar() {
-        console.log(Math.floor(Math.random() * 20 + 1))
-    }
-
-    dañar() {
-        console.log((Math.floor(Math.random() * 8 + 1)) + (Math.floor(Math.random() * 8 + 1)))
-    }
+  dañar() {
+    console.log(
+      Math.floor(Math.random() * 8 + 1) + Math.floor(Math.random() * 8 + 1)
+    );
+  }
 }
 
 const weapon1 = new Weapon("Mata dragones", "espada larga", "2d8", "cortante");
-console.log(weapon1)
-weapon1.presentar()
-weapon1.atacar()
-weapon1.dañar()
+console.log(weapon1);
+weapon1.presentar();
+weapon1.atacar();
+weapon1.dañar();
--- clase-6/arrays.js
+++ clase-6/arrays.js
@@ -1,12 +1,109 @@
-//listas
+// //listas
 
-const listaFestejo = ["bebidas", "salchichas", "pan", "torta", "mayonesa", "vodka"]
+// const skills = [
+//   "Fuerza",
+//   "Destreza",
+//   "Constitución",
+//   "Inteligencia",
+//   "Sabiduría",
+//   "Carisma",
+// ];
 
-console.log(listaFestejo);
-console.log("en la primer posición tenemos " + listaFestejo[0]);
-console.log(listaFestejo.length);
+// console.log(skills);
 
-for (let i = 0; i <= listaFestejo.length; i++) {
-    console.log(listaFestejo[i]);;
+// for (let i = 0; i < skills.length; i++) {
+//   console.log(skills[i]);
+// }
 
+// //metodo push
+// skills.push("Apariencia");
+// console.log(skills);
+
+// //metodo unshift
+// skills.unshift("Percepcion");
+// console.log(skills);
+
+// //metodo pop
+// skills.pop();
+// console.log(skills);
+
+// //metodo shift
+// skills.shift();
+// console.log(skills);
+
+// //metodo splice
+// skills.splice(3, 5);
+// console.log(skills);
+
+// skills.splice(3, 0, "Apariencia");
+// console.log(skills);
+
+// //metodo join
+// console.log(skills.join(" | "));
+
+// //metodo concat
+// const meleeWeapons = ["Espada", "Hacha", "Lanza"];
+// const rangeWeapons = ["Arco Largo", "Ballesta", "Javalina"];
+
+// console.log(meleeWeapons);
+// console.log(rangeWeapons);
+
+// const weapons = meleeWeapons.concat(rangeWeapons);
+// console.log(weapons);
+
+// //metodo slice
+// const equipedWeapons = weapons.slice(2, 4);
+// console.log(equipedWeapons);
+
+// //metodo indexOf
+
+// //metodo includes
+
+// //metodo reverse
+// console.log(weapons.reverse());
+
+//arrays de objetos
+
+// const stats = [
+//   { nombre: "fuerza", valor: 18, modificador: 4 },
+//   { nombre: "destreza", valor: 16, modificador: 3 },
+//   { nombre: "constitución", valor: 18, modificador: 4 },
+//   { nombre: "inteligencia", valor: 10, modificador: 0 },
+//   { nombre: "sabiduría", valor: 14, modificador: 2 },
+//   { nombre: "carisma", valor: 12, modificador: 1 },
+// ];
+
+// console.log(stats);
+// console.log(stats[1]);
+
+// //for of
+// for (const stat of stats) {
+//   console.log(
+//     "tu modificador de " + stat.nombre + " es de " + stat.modificador
+//   );
+// }
+
+//Crear objeto
+class Stat {
+  constructor(nombre, valor) {
+    this.nombre = nombre.toLowerCase();
+    this.valor = parseInt(valor);
+  }
+
+  calcularMod() {
+    this.mod = Math.floor((this.valor - 10) / 2);
+    console.log("tu modificador de " + this.nombre + " es de " + this.mod);
+  }
 }
+
+//Declarar array de stats a almacenar
+const stats = [];
+stats.push(new Stat("Fuerza", "18"));
+stats.push(new Stat("Destreza", "16"));
+stats.push(new Stat("Constitución", "18"));
+stats.push(new Stat("Insteligencia", "8"));
+stats.push(new Stat("Sabiduría", "12"));
+stats.push(new Stat("Carisma", "14"));
+
+//Ejecutar metodo dentro del array
+stats[5].calcularMod();
