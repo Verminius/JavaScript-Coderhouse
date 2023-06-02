33cec50497dfb0cb69a09f268e4cb78865aae210
Alexander Patiño <alpatino.ortiz@gmail.com>
2023-06-01 19:16:05 -0500

    realizadas practicas clase 8

---
+++ clase-8/math.js
@@ -0,0 +1,28 @@
+// metodo Math
+
+console.log(Math.E)
+console.log(Math.PI)
+console.log(Math.min(8, 5, 2, 90, 87, 1))
+console.log(Math.max(8, 5, 2, 90, 87, 1))
+
+//redondeado hacia arriba
+console.log(Math.ceil(3.1416))
+
+//redondeado hacia abajo
+console.log(Math.floor(3.1416))
+
+//redondeado al más cercano
+console.log(Math.round(3.1416))
+
+//raiz cuadrada
+console.log(Math.sqrt(64))
+
+//numero aleatorio entre 0 y 1
+console.log(Math.random())
+
+
+
+//clase Date
+
+console.log(new Date())
+console.log(new Date())
