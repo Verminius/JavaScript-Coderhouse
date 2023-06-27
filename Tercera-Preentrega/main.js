65e1aa82229367aeac1c163135832a2d07c549d8
Alexander Patiño <alpatino.ortiz@gmail.com>
2023-06-27 06:45:01 -0500

    preparando tercera entrega

--- Tercera-Preentrega/main.js
+++ Tercera-Preentrega/main.js
@@ -1,13 +1,76 @@
-/* Stats */
-function stats(fue, des, con, int, sab, cha) {
-    this.fue = parseInt(fue);
-    this.des = parseInt(des);
-    this.con = parseInt(con);
-    this.int = parseInt(int);
-    this.sab = parseInt(sab);
-    this.cha = parseInt(cha);
+//variables
+let btnCreate = document.getElementById("btn")
+const number = document.getElementsByClassName("number")
+    // Dados
+const dados = [4, 6, 8, 10, 12, 20]
 
-    this.calcularMod = function(stat) {
-        return Math.floor((stat - 10) / 2);
+
+// Funciones
+function lanzarDado(dado) {
+    let lanzar = Math.floor((Math.random() * dado) + 1)
+    return lanzar
+}
+
+function calcularStat() {
+    let sumaD6 = [lanzarDado(dados[1]),
+        lanzarDado(dados[1]),
+        lanzarDado(dados[1]),
+        lanzarDado(dados[1]),
+    ]
+
+
+
+    sumaD6.sort((a, b) => a - b)
+        // console.log(sumaD6)
+
+    sumaD6.shift(0)
+        // console.log(sumaD6)
+
+    let total = sumaD6.reduce((acc, num) => {
+        return acc + num;
+    })
+
+    return total
+
+
+}
+
+
+//eventos
+
+btnCreate.addEventListener("click", () => {
+    setTimeout(() => {
+        location.assign("/Tercera-Preentrega/crear-personaje.html")
+    }, 500);
+})
+
+function crearNuevoPersonaje() {
+    const nombre = document.getElementById("char-name").value;
+    const raza = document.getElementsByClassName("raza").value;
+    const clas = document.getElementsByClassName("clase").value;
+    const fuerza = parseInt(document.getElementById("fuerza").value);
+    const destreza = parseInt(document.getElementById("destreza").value);
+    const constitucion = parseInt(document.getElementById("constitucion").value);
+    const inteligencia = parseInt(document.getElementById("inteligencia").value);
+    const sabiduria = parseInt(document.getElementById("sabiduria").value);
+    const carisma = parseInt(document.getElementById("carisma").value);
+
+    const stat = new Stat(fuerza, destreza, constitucion, inteligencia, sabiduria, carisma);
+    const personaje = new Personaje(nombre, raza, clase, stat);
+
+
+    let personajesExistentes = localStorage.getItem("personajes");
+    if (personajesExistentes) {
+        personajesExistentes = JSON.parse(personajesExistentes);
+    } else {
+        personajesExistentes = [];
     }
+
+
+    personajesExistentes.push(personaje);
+
+
+    localStorage.setItem("personajes", JSON.stringify(personajesExistentes));
+
+    console.log("Personaje guardado en localStorage:", personaje);
 }
---
+++ Tercera-Preentrega/objetos.js
@@ -0,0 +1,48 @@
+//objeto stats
+class Stats {
+    constructor(str, dex, con, int, wis, cha) {
+        this.str = str;
+        this.dex = dex;
+        this.con = con;
+        this.int = int;
+        this.wis = wis;
+        this.cha = cha;
+    }
+
+    // Métodos 
+    calcularModificador(estadistica) {
+        return Math.floor((estadistica - 10) / 2);
+    }
+}
+
+//objeto raza
+class Raza {
+    constructor(descripcion, ASI, tamaño, velocidad, lenguaje) {
+        this.descripcion = descripcion;
+        this.ASI = ASI;
+        this.tamaño = tamaño;
+        this.velocidad = velocidad;
+        this.lenguaje = lenguaje
+    }
+}
+
+
+//razas
+const humano = new Raza("Los humanos son la más joven de las razas comunes, con una llegada tardía al mundo y una vida corta en comparación con los enanos, elfos, y dragones. Quizás es por su corta vida que se esfuerzan en conseguir tanto como pueden en los años que se les han dado. O quizás sienten que tienen algo que demostrar a las razas más antiguas, y es por eso que construyen poderosos imperios basados en la conquista y el comercio. Sea lo que sea lo que les motiva, los humanos son los innovadores, los pioneros y los triunfadores de los mundos", 1, "Mediano", 30, "común")
+
+const enano = new Raza("Reinos ricos en antiguo esplendor, salas esculpidas en las raíces de las montañas, picos y martillos haciéndose eco en profundas minas y ardientes forjas, un compromiso con el clan y las tradiciones y un odio ardiente hacia orcos y goblins —estos temas comunes unen a todos los enanos", 1, "Mediano", 25, "común y enano")
+
+const elfo = new Raza("Los elfos son un pueblo mágico de gracia sobrenatural, viviendo en el mundo sin ser del todo parte de él. Viven en lugares de etérea belleza, en medio de antiguos bosques o en plateados minaretes que resplandecen con luz feérica, donde una suave música flota en el aire y delicadas fragancias son transportadas por la brisa. Los elfos aman la naturaleza y la magia, el arte y la maestría, la música y la poesía, y las cosas buenas del mundo.", 1, "Mediano", 30, "común y elfico")
+
+const razas = [humano, enano, elfo]
+
+
+//obtejo personaje
+class Personaje {
+    constructor(nombre, objetoRaza, objetoClase, objetoStats) {
+        this.nombre = nombre;
+        this.objetoRaza = objetoRaza;
+        this.objetoClase = objetoClase;
+        this.objetoStats = objetoStats;
+    }
+};
